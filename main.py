832448d1e14fa1daef33b1d1daa84153f5c022eb
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-10-19 22:21:52 +0800

    s行閃燈

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ X = 0
 
 def on_forever():
     global Y, X
-    while True:
-        Y = 4
-        while Y <= 4:
+    Y = 4
+    while Y <= 4:
+        if Y % 2 == 0:
             X = 4
             while X <= 4:
                 led.plot(X, Y)
                 basic.pause(500)
                 X += -1
-            Y += -1
-        basic.clear_screen()
+                if X < 0:
+                    Y += -1
+                    break
+        else:
+            while Y <= 4:
+                X = 0
+                while X <= 4:
+                    led.plot(X, Y)
+                    basic.pause(500)
+                    X += 1
+                    if X > 4:
+                        Y += -1
+                        break
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,13 +3,24 @@ let X = 0
 basic.forever(function () {
     Y = 4
     while (Y <= 4) {
-        X = 4
-        if (true) {
+        if (Y % 2 == 0) {
+            X = 4
             while (X <= 4) {
                 led.plot(X, Y)
                 basic.pause(500)
                 X += -1
                 if (X < 0) {
+                    Y += -1
+                    break;
+                }
+            }
+        } else {
+            X = 0
+            while (X <= 4) {
+                led.plot(X, Y)
+                basic.pause(500)
+                X += 1
+                if (X > 4) {
                     Y += -1
                     break;
                 }
